a5ace1b095bbdaa071db24cdaa9760d52ff57eeb
Ravindra Kumar <rkumar6821@gmail.com>
2021-05-01 18:33:32 +0530

    PORT01

--- script/navigation.js
+++ script/navigation.js
@@ -1,19 +1,6 @@
-function myFunction() {
-          var x = document.getElementById("myTopnav");
-          if (x.className === "topnav") {
-            x.className += " responsive";
-          } else {
-            x.className = "topnav";
-          }
-}
+const toggleBtn = document.getElementsByClassName('togglebtn')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 
-window.onscroll = function() {scrollFunction()};
-function scrollFunction() {
-  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-    document.getElementById("myTopnav").style.backgroundColor = "#fff";
-    document.getElementById("myTopnav a").style.color = "#000";
-  
-  } else {
-    document.getElementById("myTopnav").style.background = "none";
-  }
-}
+toggleBtn.addEventListener('click', () => {
+    navbarLinks.classList.toggle('active')
+})
